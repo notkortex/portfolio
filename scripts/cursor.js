c4031426842907a8e01ce1e7bcc7ae4a0f1a3135
nine4x <118857854+nine4x@users.noreply.github.com>
2023-03-23 01:44:07 +0000

    format html css & js

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -3,27 +3,27 @@ const links = document.querySelectorAll("li a");
 const navlinks = document.querySelectorAll("nav ul li");
 
 document.addEventListener("mousemove", (e) => {
-    let leftPosition = e.pageX + 4;
-    let topPosition = e.pageY + 4;
+   let leftPosition = e.pageX + 4;
+   let topPosition = e.pageY + 4;
 
-    cursor.style.left = leftPosition + "px";
-    cursor.style.top = topPosition + "px";
+   cursor.style.left = leftPosition + "px";
+   cursor.style.top = topPosition + "px";
 })
 
 links.forEach(link => {
-    link.addEventListener("mouseenter", () => {
-        cursor.classList.add("large");
-    })
+   link.addEventListener("mouseenter", () => {
+      cursor.classList.add("large");
+   })
 })
 
 links.forEach(link => {
-    link.addEventListener("mouseleave", () => {
-        cursor.classList.remove("large");
-    })
+   link.addEventListener("mouseleave", () => {
+      cursor.classList.remove("large");
+   })
 })
 
 // Animation
 
 navlinks.forEach((li, i) => {
-    li.style.animationDelay = 0 + i * 140 + "ms";
+   li.style.animationDelay = 0 + i * 140 + "ms";
 })
