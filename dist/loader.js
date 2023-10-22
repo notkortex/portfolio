80bf87922d6fa4e1327f11ea7ba6e73bd31d5252
kortex <118857854+notkortex@users.noreply.github.com>
2023-10-22 16:01:27 +0000

    clean up loader

--- dist/loader.js
+++ dist/loader.js
@@ -1,46 +1,28 @@
 function playclip() {
-  if (
-    (navigator.appName == "Microsoft Internet Explorer" &&
-      navigator.appVersion.indexOf("MSIE 7") != -1) ||
-    navigator.appVersion.indexOf("MSIE 8") != -1
-  ) {
-    if (document.all) {
-      document.all.sound.src = "song.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementsByTagName("audio");
+  var audio = document.querySelector("audio");
 
-      if (audio.length > 0) audio[0].play();
-
-      var video = document.getElementsByTagName("video");
-
-      if (video.length > 0) {
-        video = video[0];
-        video.removeAttribute("hidden");
-        video.play();
-      }
-    }
+  if (audio) {
+    audio.play();
   }
 }
 
+// Add a click event listener to elements with the "song" class to play music
 document.querySelector(".song").addEventListener("click", function () {
   playclip();
 });
 
+// Use jQuery to ready document
 $(document).ready(function () {
+  // Add a click event listener to the document body to load fade animation
   $(document.body).click(function () {
     $(".clickanywhere").fadeOut(1000);
-    $(".biolink").fadeIn(1000);
 
+    // Define and play function with "audio" ID
     let play = function () {
-      document.getElementById("audio").play();
+      var audio = document.getElementById("audio");
+      if (audio) {
+        audio.play();
+      }
     };
   });
 });
-
-var _old = jQuery.Event.prototype.stopPropagation;
-
-jQuery.Event.prototype.stopPropagation = function () {
-  this.target.nodeName !== "SPAN" && _old.apply(this, arguments);
-};
